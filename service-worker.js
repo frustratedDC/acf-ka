1136f8b9cc5e7f9eadfac2272d4f3cbdc44f2473
frustratedDC <3872allma@armymail.mod.uk>
2026-02-21 18:50:12 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,25 @@
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("ka-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "activation.html",
-        "activation.js",
-        "config.js",
-        "graphAuth.js",
-        "graphUpload.js",
-        "completion.html",
-        "admin.html",
-        "session.html",
-        "attendance.html",
-        "scores.html",
-        "progression.html",
-        "results.html",
-        "logbook.html",
-        "style.css",
-        "manifest.json"
-      ]);
-    })
-  );
-  self.skipWaiting(); // ensure new SW activates immediately
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys
-          .filter(key => key !== "ka-cache")
-          .map(key => caches.delete(key))
-      );
-    })
-  );
-  self.clients.claim(); // take control of all pages immediately
+    event.waitUntil(
+        caches.open("ka-cache").then(cache => {
+            return cache.addAll([
+                "index.html",
+                "attendance.html",
+                "scores.html",
+                "completion.html",
+                "style.css",
+                "graphUpload.js",
+                "graphAuth.js",
+                "activation.js",
+                "manifest.json"
+            ]);
+        })
+    );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
